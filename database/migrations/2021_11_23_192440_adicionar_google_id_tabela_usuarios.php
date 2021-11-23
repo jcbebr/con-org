2003944b33f3506f413770174736bdaaeafd9c5f
José Carlos <jcbebr@gmail.com>
2021-11-23 17:44:37 -0300

    Ajuste migration google_id

--- database/migrations/2021_11_23_192440_adicionar_google_id_tabela_usuarios.php
+++ database/migrations/2021_11_23_192440_adicionar_google_id_tabela_usuarios.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddGoogleIdInUsersTable extends Migration
+class AdicionarGoogleIdTabelaUsuarios extends Migration
 {
     /**
      * Run the migrations.
