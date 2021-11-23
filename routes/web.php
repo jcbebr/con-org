6f39c08bb53016bb8095457520da57f43f4faaf0
José Carlos <jcbebr@gmail.com>
2021-11-23 13:23:17 -0300

    Primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $users = User::paginate(10);
+    return view('welcome')->with([
+        'users' => $users
+    ]);
+});
