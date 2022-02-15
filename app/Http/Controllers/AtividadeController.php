e28fef6013adf4696244d7bce3c0f6256b301141
José Carlos <jcbebr@gmail.com>
2022-02-15 15:49:21 -0300

    US06 salvamento temporário da descrição da atividade e alterações gerais no rodapé

--- app/Http/Controllers/AtividadeController.php
+++ app/Http/Controllers/AtividadeController.php
@@ -7,5 +7,20 @@ use TCG\Voyager\Http\Controllers\VoyagerBaseController;
 
 class AtividadeController extends VoyagerBaseController
 {
-    //
+
+    public function atividade_salvar_descricao(Request $request)
+    {
+        $request->session()->put('desc_atividade', $request->data);
+    }
+
+    public function atividade_recuperar_descricao(Request $request)
+    {
+        echo $request->session()->get('desc_atividade');
+    }
+
+    public function store(Request $request) 
+    {
+        $request->session()->put('desc_atividade', '');
+        return parent::store($request);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::group(['prefix' => '/'], function () {
 Route::group(['middleware' => 'admin.user'], function () {
     Route::get('painel', ['uses' => 'App\Http\Controllers\PainelController@index', 'as' => 'index']);
 
+    Route::get('atividade/descricao/recuperar', ['uses' => 'App\Http\Controllers\AtividadeController@atividade_recuperar_descricao', 'as' => 'atividade_recuperar_descricao']);
+    Route::post('atividade/descricao/salvar', ['uses' => 'App\Http\Controllers\AtividadeController@atividade_salvar_descricao', 'as' => 'atividade_salvar_descricao']);
     Route::resource('atividade/{atividade_id}/avaliacao', 'App\Http\Controllers\AvaliacaoAtividadeController', ['parameters' => ['avaliacao' => 'id' , 'atividade_id' => 'av_id']]);
     Route::resource('roteiro/{roteiro_id}/roteiro-atividade', 'App\Http\Controllers\RoteiroAtividadeController', ['parameters' => ['roteiro-atividade' => 'id' , 'roteiro_id' => 'roteiro_id']]);
 });
